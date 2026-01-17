5ca1ff0e86fad226a066c218fac0520e3040a2b5
Animacypher <sj2404629@gmail.com>
2026-01-17 22:24:42 +0800

    Day 3

---
+++ String methods/index.js
@@ -0,0 +1,16 @@
+// string methonds = allow you to manipulate and work with text (strings)
+
+let userName = "Brocode  ";
+
+console.log(userName.charAt(0));
+console.log(userName.indexOf("o"));
+console.log(userName.lastIndexOf("o"));
+console.log(userName.length);
+
+//userName = userName.trim();
+//userName = userName.toUpperCase();
+//userName = userName.toLowerCase();
+//userName = userName.repeat(3);
+
+
+console.log(userName);
