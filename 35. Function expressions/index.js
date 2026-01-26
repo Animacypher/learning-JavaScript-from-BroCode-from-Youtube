69ce79765f4630ebcdb7d27b82b01686d8aa2096
Animacypher <sj2404629@gmail.com>
2026-01-26 23:23:18 +0800

    Day 7

---
+++ 35. Function expressions/index.js
@@ -0,0 +1,11 @@
+// function declaration = define a reusable block of code that performs a specific task
+// function expressions = a way to define functions as values or variables
+
+/*const hello = function(){
+    console.log("Hello")
+}
+hello();*/
+
+setTimeout(function(){
+    console.log("hello");
+}, 3000);
